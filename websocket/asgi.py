00bbd6d509a680ef18b78cd1f2ee135e1a5f0f99
Christoph <c.wagner.germany@gmail.com>
2025-06-09 15:20:14 +0200

    final

--- websocket/asgi.py
+++ websocket/asgi.py
@@ -2,16 +2,12 @@
 ASGI config for websocket project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from chat.routing import websocket_urlpatterns
 
@@ -22,7 +18,8 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
     ),
-    # Just HTTP for now. (We can add other protocols later.)
 })
--- websocket/settings.py
+++ websocket/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'websocket.wsgi.application'
 ASGI_APPLICATION = "websocket.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
